a9b60e03269b2f0c5ee9e5c7c7840e87345acc99
Michael Scholz <michael@wellthapp.com>
2025-03-04 16:30:30 -0800

    fixed it myself, thanks

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -2,41 +2,42 @@ import type {Route} from "./+types/login";
 import {useGoogleLogin} from '@react-oauth/google';
 import {useFetcher, useNavigate} from "react-router";
 import {OAuth2Client} from "google-auth-library";
+import {useEffect} from "react";
 
-export function action(actionArgs: Route.ActionArgs) {
-  // const request = actionArgs.request;
-  // const data = await request.json();
-  // const oAuth2Client = new OAuth2Client(
-  //   process.env.CLIENT_ID,
-  //   process.env.CLIENT_SECRET,
-  //   'postmessage',
-  // );
-  // const code = data.code;
-  // const {tokens} = await oAuth2Client.getToken(data.code); // exchange code for tokens
-  // if (tokens?.id_token) {
-  //   const ticket = await oAuth2Client.verifyIdToken({
-  //     idToken: tokens.id_token,
-  //   });
-  //   if (ticket) {
-  //     const payload = ticket.getPayload();
-  //     if (payload?.email_verified) {
-  //       return {route: "/home"};
-  //     }
-  //   }
-  // }
-  return {route:"/home"};
+export async function action(actionArgs: Route.ActionArgs) {
+  const request = actionArgs.request;
+  const data = await request.json();
+  const oAuth2Client = new OAuth2Client(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    'postmessage',
+  );
+  const code = data.code;
+  const {tokens} = await oAuth2Client.getToken(data.code); // exchange code for tokens
+  if (tokens?.id_token) {
+    const ticket = await oAuth2Client.verifyIdToken({
+      idToken: tokens.id_token,
+    });
+    if (ticket) {
+      const payload = ticket.getPayload();
+      if (payload?.email_verified) {
+        return {route: "home"};
+      }
+    }
+  }
 }
 
 export default function Login({loaderData, actionData}: Route.ComponentProps) {
   const navigate = useNavigate();
-  console.log(`Login: loaderData ${loaderData}, actionData ${actionData}`)
-  if (actionData && actionData.route) {
-    navigate(actionData.route);
-  }
   const fetcher = useFetcher();
+  useEffect(() => {
+    if (fetcher.data && fetcher.data.route) {
+      navigate(fetcher.data.route);
+    }
+  }, [fetcher.data]);
   const googleLogin = useGoogleLogin({
     onSuccess: async ({code}) => {
-      const response = await fetcher.submit({code}, {
+      await fetcher.submit({code}, {
         method: "post",
         encType: "application/json"
       });
